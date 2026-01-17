b042ab4ae86bd369f9539ea184e693fd0d178677
Julio Cesar <julioreis951@gmail.com>
2026-01-16 22:37:33 -0300

    Exercícios da aula 01

---
+++ exercicios/capturaElementos.js
@@ -0,0 +1,20 @@
+const html1 = document.querySelector('html');
+const timer = document.querySelector('#timer');
+const image = document.querySelector('.app__image');
+const title = document.querySelector('.app__title');
+
+console.log('HTML:', html);
+console.log('Timer', timer);
+console.log('Image', image);
+console.log('Title', title);
+
+const iniciarPausarTimer = document.querySelector('#start-pause');
+const focoButton = document.querySelector('.app__card-button--foco');
+const descansoCurtoButton = document.querySelector('.app__card-button--curto');
+const descansoLongoButton = document.querySelector('.app__card-button--longo');
+
+const tempoFoco = 1500;
+const tempoDescansoCurto = 300;
+const tempoDescansoLongo = 900;
+
+
