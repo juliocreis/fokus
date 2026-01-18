493673f5434838cc1b9bef400f94d0b8a84ba3d0
Julio Cesar <julioreis951@gmail.com>
2026-01-18 11:15:36 -0300

    Altera titulo principal da pagina ao clicar nos botoes

--- script.js
+++ script.js
@@ -3,20 +3,28 @@ const focoBt = document.querySelector('.app__card-button--foco');
 const descansoCurtoBt = document.querySelector('.app__card-button--curto');
 const descansoLongoBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
+const titulo = document.querySelector('.app__title');
 
 focoBt.addEventListener('click', () => {
     alteraContexto('foco');
+    alteraTitulo('Otimize sua produtividade, <strong class="app__title-strong">mergulhe no que importa.</strong>');
 });
 
 descansoCurtoBt.addEventListener('click', () => {
     alteraContexto('descanso-curto');
+    alteraTitulo('Que tal dar uma respirada? <strong class="app__title-strong">Faça uma pausa curta!</strong>');
 });
 
 descansoLongoBt.addEventListener('click', () => {
     alteraContexto('descanso-longo');
+    alteraTitulo('Hora de voltar à superfície. <strong class="app__title-strong">Faça uma pausa longa.</strong>');
 });
 
 function alteraContexto(contexto) {
     html.setAttribute('data-contexto', contexto);
     banner.setAttribute('src', `./imagens/${contexto}.png`);
+}
+
+function alteraTitulo(texto) {
+    titulo.innerHTML = texto;
 }
