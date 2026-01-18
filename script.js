f2977dbc0bfde786065f83bfe44f328b423d4916
Julio Cesar <julioreis951@gmail.com>
2026-01-18 10:36:14 -0300

    Refatora codigo de alterar contexto

--- script.js
+++ script.js
@@ -5,16 +5,18 @@ const descansoLongoBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 
 focoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'foco');
-    banner.setAttribute('src', './imagens/foco.png');
+    alteraContexto('foco');
 });
 
 descansoCurtoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-curto');
-    banner.setAttribute('src', './imagens/descanso-curto.png');
+    alteraContexto('descanso-curto');
 });
 
 descansoLongoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-longo');
-    banner.setAttribute('src', './imagens/descanso-longo.png');
+    alteraContexto('descanso-longo');
 });
+
+function alteraContexto(contexto) {
+    html.setAttribute('data-contexto', contexto);
+    banner.setAttribute('src', `./imagens/${contexto}.png`);
+}
