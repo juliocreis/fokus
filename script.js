a7ea20469b85e3f39588202496e2bf1d72d5b04f
Julio Cesar <julioreis951@gmail.com>
2026-01-18 10:21:32 -0300

    Altera imagens ao clicar nos botoes de foco e descanso

--- script.js
+++ script.js
@@ -2,15 +2,19 @@ const html = document.querySelector('html');
 const focoBt = document.querySelector('.app__card-button--foco');
 const descansoCurtoBt = document.querySelector('.app__card-button--curto');
 const descansoLongoBt = document.querySelector('.app__card-button--longo');
+const banner = document.querySelector('.app__image');
 
 focoBt.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'foco');
+    banner.setAttribute('src', './imagens/foco.png');
 });
 
 descansoCurtoBt.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-curto');
+    banner.setAttribute('src', './imagens/descanso-curto.png');
 });
 
 descansoLongoBt.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-longo');
+    banner.setAttribute('src', './imagens/descanso-longo.png');
 });
